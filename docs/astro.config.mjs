ad98db3b1e1502123e6f78d03238b899d9017267
Pratyush Tiwary <pratyushtiwary33@gmail.com>
2024-06-08 15:36:00 +0530

    Added sharp

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,11 +1,8 @@
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  image: {
-    service: passthroughImageService(),
-  },
   integrations: [
     starlight({
       title: "ToyNN Docs",
